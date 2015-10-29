92275935b78507298a68c2cbb2c438299ccd8142
shenyufu <shenyufu@gmail.com>
2015-10-29 15:01:53 +0800

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -20,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertIn('To-Do', header_text)
 
 		# 
-		inputbox = self.browser.fine_element_by_id('id_new_item')
+		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(
 				inputbox.get_attribute('placeholder'),
 				'Enter a to-do item'
@@ -32,9 +33,10 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 
 		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_element_by_tag_name('tr')
+		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)		
+			any(row.text == '1: Buy peacock feathers' for row in rows),
+			"New to-do item did not appear in table"
 		)
 
 		self.fail('Finish the test!')

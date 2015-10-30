5c83a7a76488f465032c022bd42300f86b26b782
shenyufu <shenyufu@gmail.com>
2015-10-30 08:50:20 +0800

    test

--- functional_tests.py
+++ functional_tests.py
@@ -27,16 +27,25 @@ class NewVisitorTest(unittest.TestCase):
 				'Enter a to-do item'
 		)
 
-		# 
-		inputbox.send_keys('Buy peacock feathers')
+		# first to-do
+#		inputbox.send_keys('Buy peacock feathers')
+#		inputbox.send_keys(Keys.ENTER)
+	
+#		table = self.browser.find_element_by_id('id_list_table')
+#		rows = table.find_elements_by_tag_name('tr')
+#		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
+		# Second to-do
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-
+	
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows),
-			"New to-do item did not appear in table"
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn(
+			'2: Use peacock feathers to make a fly', 
+			[row.text for row in rows])
 		)
 
 		self.fail('Finish the test!')

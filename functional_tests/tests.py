a044a2f104e1e42c2b216b7b8ed13d0bd2ea33e1
shenyufu <shenyufu@gmail.com>
2015-11-02 15:17:18 +0800

    new URL

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -66,6 +66,8 @@ class NewVisitorTest(LiveServerTestCase):
 
 		francis_list_url = self.browser.current_url
 		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url, edith_list_url)
+
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertIn('Buy milk', page_text)
